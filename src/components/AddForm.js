a19ccec9eba0009385beaf99c6264f71aa39bec7
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-10-16 11:20:53 -0400

    Added gradient background

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,27 +1,27 @@
 /*import PropTypes from 'prop-types'*/
-import { useState } from 'react'
-import Button from './Button'
+import { useState } from 'react';
+import Button from './Button';
 const AddForm = ({}) => {
-    const [text, setText] = useState('')
-    const onAdd = (e) => {
-        //onClick({text})
-        e.preventDefault();
-        console.log(text)
-    } 
-    return (
-        <form className='add-form'>
-            <div className='form-control' />
-            <label />
-                Location:
-            <input
-                type='text'
-                placeholder='Enter location' 
-                value = {text}
-                onChange={(e) => setText(e.target.value)}
-                />
-            <Button onClick = {onAdd}/>
-        </form>
-    )
-}
+  const [text, setText] = useState('');
+  const onAdd = (e) => {
+    //onClick({text})
+    e.preventDefault();
+    console.log(text);
+  };
+  return (
+    <form className="add-form">
+      <div className="form-control" />
+      <label />
+      Location:
+      <input
+        type="text"
+        placeholder="Enter location"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <Button onClick={onAdd} />
+    </form>
+  );
+};
 
-export default AddForm
+export default AddForm;
