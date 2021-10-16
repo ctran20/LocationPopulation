102ef4b402853badec6d47a6e9a34740945f6a15
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-10-16 12:06:16 -0400

    Set up population display placeholder
    Using random int as placeholder for some population api

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 const App = () => {
   const location = '';
   return (
-    <div className="app">
-      <h1 className="ma3">Location Population</h1>
-      <AddForm form1></AddForm>
+    <div>
+      <div className="ma4">
+        <h1 className="f1 lh-title">Location Population</h1>
+        <AddForm form1></AddForm>
+      </div>
     </div>
   );
 };
--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,26 +1,45 @@
 /*import PropTypes from 'prop-types'*/
 import { useState } from 'react';
-import Button from './Button';
 const AddForm = ({}) => {
   const [text, setText] = useState('');
+  const [showText, setShowText] = useState('');
   const onAdd = (e) => {
     //onClick({text})
     e.preventDefault();
     console.log(text);
+
+    setShowText(
+      `There are ${
+        Math.floor(Math.random() * 400) + 1
+      },000 people living in ${text}.`
+    );
   };
   return (
-    <form className="add-form">
-      <div className="form-control" />
-      <label />
-      Location:
-      <input
-        type="text"
-        placeholder="Enter location"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <Button onClick={onAdd} />
-    </form>
+    <div>
+      <form className="add-form">
+        <div className="form-control" />
+
+        <input
+          className="pa3"
+          type="text"
+          placeholder="Enter location"
+          style={{ width: 300 }}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button
+          style={{ width: 150 }}
+          className="pa3"
+          type="submit"
+          onClick={onAdd}
+        >
+          Switch Mode
+        </button>
+      </form>
+
+      {/*Temp*/}
+      <div className="mv4 f4 lh-copy">{showText}</div>
+    </div>
   );
 };
 
