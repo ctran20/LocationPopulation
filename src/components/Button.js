d691f484292646942dcbff187e237b7d1790e633
wzhangc1 <wzhangc1@binghamton.edu>
2021-10-16 00:27:50 -0400

    Added basic eventHandlers for textbox

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+/*import PropTypes from 'prop-types'*/
+const Button = ({onClick}) => {
+    /*const onAdd = (e) => {
+        console.log('Button Pressed')
+    }*/
+    return (
+        <button
+            onClick={onClick}
+            > Search
+        </button>
+    )
+}
+
+export default Button
